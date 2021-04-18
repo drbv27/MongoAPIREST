2b21e1345c7dac8dae5fcef71ac8161c65819ba6
Diego Bonilla <drbv27@gmail.com>
2021-04-18 17:00:46 -0500

    Install bodyParser

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+const port = process.env.PORT || 3000
 
-app.listen(3000, ()=>{
-    console.log('API REST corriendo en: http://localhost:3000')
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`API REST corriendo en: http://localhost:${port}`)
 })
