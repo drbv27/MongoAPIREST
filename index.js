fa61e89af13226eb52e4752c75fa917c5ae2a52a
Diego Bonilla <drbv27@gmail.com>
2021-04-19 17:57:58 -0500

    GET all Array

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.get('/api/product', (req,res) => {
-    res.status(200).send({products: []})
+    Product.find({}, (err, products) => {
+    if (err) return res.status(500).send({message: `Error al realizar la peticion: ${err}`})
+    if (!products) return res.status(404).send({message: `No existen Productos`})
+    
+    res.status(200).send({products: products})
+    })
 })
 
 app.get('/api/product/:productId', (req,res) => {
