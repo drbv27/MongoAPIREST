88131f630846d98fe1efcb4246a12f4be7d1c562
Diego Bonilla <drbv27@gmail.com>
2021-04-19 05:50:18 -0500

    Creating Request and proving GET and POST

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const port = process.env.PORT || 3001
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
+app.get('/api/product', (req,res) => {
+    res.status(200).send({products: []})
+})
+
+app.get('/api/product/:productId', (req,res) => {
+    
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: `El producto se ha recibido`})
+})
+
+app.put('/api/product/:productId', (req,res) => {
+
+})
+
+app.delete('/api/product/productId', (req, res) => {
+
+})
+
 app.listen(port, ()=>{
     console.log(`API REST corriendo en: http://localhost:${port}`)
 })
