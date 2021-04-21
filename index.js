41afc69aebf22dc67ebe6c5d2e5e470b3f9960db
Diego Bonilla <drbv27@gmail.com>
2021-04-21 08:09:20 -0500

    Refactoring 3. Config.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 
 const mongoose = require('mongoose')
 const app = require('./app')
-const port = process.env.PORT || 3001
+const config = require('./config')
 
-mongoose.connect('mongodb://localhost:27017/shopi',{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err, res) => {
+mongoose.connect( config.db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err, res) => {
     if (err) {
         return console.log(`Error al conectar a la DB: ${err}`)
     }
     console.log('Conexion a la DB Ok!...')
 
-    app.listen(port, ()=>{
-        console.log(`API REST corriendo en: http://localhost:${port}`)
+    app.listen(config.port, ()=>{
+        console.log(`API REST corriendo en: http://localhost:${config.port}`)
     })
 })
